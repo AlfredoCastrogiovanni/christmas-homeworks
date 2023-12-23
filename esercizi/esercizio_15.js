a31059d6a9af8bd353007d7eeed653dd45ef1c71
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-12-23 11:52:33 +0100

    es15: completato

--- esercizi/esercizio_15.js
+++ esercizi/esercizio_15.js
@@ -5,7 +5,8 @@
 
 const numbers = [10, 43, -34, -2, 78];
 let average = 0;
-for (let i = _; i < _; _) {
-    _
+for (let i = 0; i < numbers.length ; i++) {
+    average += numbers[i];
+    average = i == numbers.length - 1 ? average / numbers.length : average;
 }
 console.log('Esercizio 15', average);
