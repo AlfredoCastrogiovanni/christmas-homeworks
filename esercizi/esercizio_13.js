88e5a375c7eeb39c7924bd1ae84ec1cebe2a2b82
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-12-23 11:42:06 +0100

    es:13 completato

--- esercizi/esercizio_13.js
+++ esercizi/esercizio_13.js
@@ -5,5 +5,18 @@
  * finisca per contenere una griglia 20x20 di <div> celle, ciascuno con dentro uno <span>
  */
 
-const element = document.querySelector(_);
-_
+const element = document.querySelector("#contenitore");
+
+const cell = document.createElement("div");
+const span = document.createElement("span");
+
+cell.append(span);
+
+for(let y=0; y < 20 ; y++) {
+    for(let x=0; x < 20 ; x++) {
+        const cell = document.createElement("div");
+        const span = document.createElement("span");
+        cell.append(span);
+        element.append(cell);
+    }
+}
